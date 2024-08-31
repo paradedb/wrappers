5ece843712a4a662886b1e4d1ef95b3e0b2645c7
Ming <ming.ying.nyc@gmail.com>
2024-08-31 14:45:15 -0400

    `JsonB` and `Json` cell types (#16)

--- supabase-wrappers/src/interface.rs
+++ supabase-wrappers/src/interface.rs
@@ -8,8 +8,8 @@ use pgrx::{
     datum::Uuid,
     fcinfo,
     pg_sys::{self, BuiltinOid, Datum, Oid},
-    varlena_to_byte_slice, AllocatedByRust, AnyNumeric, FromDatum, IntoDatum, JsonB, PgBuiltInOids,
-    PgOid,
+    varlena_to_byte_slice, AllocatedByRust, AnyNumeric, FromDatum, IntoDatum, Json, JsonB,
+    PgBuiltInOids, PgOid,
 };
 use std::collections::HashMap;
 use std::ffi::CStr;
@@ -55,8 +55,9 @@ pub enum Cell {
     Time(Time),
     Timestamp(Timestamp),
     Timestamptz(TimestampWithTimeZone),
+    Json(Json),
+    JsonB(JsonB),
     Interval(Interval),
-    Json(JsonB),
     Bytea(*mut pg_sys::varlena),
     Uuid(Uuid),
     BoolArray(Vec<Option<bool>>),
@@ -84,8 +85,8 @@ impl Clone for Cell {
             Cell::Time(v) => Cell::Time(*v),
             Cell::Timestamp(v) => Cell::Timestamp(*v),
             Cell::Timestamptz(v) => Cell::Timestamptz(*v),
-            Cell::Interval(v) => Cell::Interval(*v),
-            Cell::Json(v) => Cell::Json(JsonB(v.0.clone())),
+            Cell::Json(v) => Cell::Json(Json(v.0.clone())),
+            Cell::JsonB(v) => Cell::JsonB(JsonB(v.0.clone())),
             Cell::Interval(v) => Cell::Interval(v.clone()),
             Cell::Bytea(v) => Cell::Bytea(*v as *mut pg_sys::varlena),
             Cell::Uuid(v) => Cell::Uuid(v.clone()),
@@ -176,6 +177,7 @@ impl fmt::Display for Cell {
                 )
             },
             Cell::Json(v) => write!(f, "{:?}", v),
+            Cell::JsonB(v) => write!(f, "{:?}", v),
             Cell::Interval(v) => unsafe {
                 let i = fcinfo::direct_function_call_as_datum(
                     pg_sys::interval_out,
@@ -245,6 +247,7 @@ impl IntoDatum for Cell {
             Cell::Timestamptz(v) => v.into_datum(),
             Cell::Interval(v) => v.into_datum(),
             Cell::Json(v) => v.into_datum(),
+            Cell::JsonB(v) => v.into_datum(),
             Cell::Interval(v) => v.into_datum(),
             Cell::Bytea(v) => Some(Datum::from(v as *mut pg_sys::varlena)),
             Cell::Uuid(v) => v.into_datum(),
@@ -277,7 +280,7 @@ impl IntoDatum for Cell {
             || other == pg_sys::TIMEOID
             || other == pg_sys::TIMESTAMPOID
             || other == pg_sys::TIMESTAMPTZOID
-            || other == pg_sys::INTERVALOID
+            || other == pg_sys::JSONOID
             || other == pg_sys::JSONBOID
             || other == pg_sys::INTERVALOID
             || other == pg_sys::BYTEAOID
@@ -339,8 +342,11 @@ impl FromDatum for Cell {
             PgOid::BuiltIn(PgBuiltInOids::INTERVALOID) => {
                 Interval::from_datum(datum, is_null).map(Cell::Interval)
             }
+            PgOid::BuiltIn(PgBuiltInOids::JSONOID) => {
+                Json::from_datum(datum, is_null).map(Cell::Json)
+            }
             PgOid::BuiltIn(PgBuiltInOids::JSONBOID) => {
-                JsonB::from_datum(datum, is_null).map(Cell::Json)
+                JsonB::from_datum(datum, is_null).map(Cell::JsonB)
             }
             PgOid::BuiltIn(PgBuiltInOids::BYTEAOID) => {
                 Some(Cell::Bytea(datum.cast_mut_ptr::<pg_sys::varlena>()))
